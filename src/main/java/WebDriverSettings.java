483069d9b5f9e0e4b552f2d7967e59c1e2b02bb4
Rznec <49557431+Rznec@users.noreply.github.com>
2020-04-06 14:38:23 +0300

    Update WebDriverSettings.java

--- src/main/java/WebDriverSettings.java
+++ src/main/java/WebDriverSettings.java
@@ -7,17 +7,19 @@ import java.util.logging.Logger;
 
 public class WebDriverSettings {
     public static ChromeDriver driver;
+    public static final Logger logger= Logger.getLogger(WebDriverSettings.class.getName());
 
     @BeforeAll
     static void setUp() {
         System.setProperty("webdriver.chrome.driver", "F:\\Tinkoff/chromedriver.exe");
         driver = new ChromeDriver();
-        //System.out.println("Test start");
+        System.out.println("");
+        logger.info("\nTest start");
     }
 
     @AfterAll
     static void close() {
-        System.out.println("Test close");
+        logger.info("\nTest start");
         driver.quit();
     }
 }
